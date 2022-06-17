0ef1df4b05acd5ebe9be7be6b9bb4ec3c11dd1d0
ekta-013 <ektamo0123@gmail.com>
2022-06-17 17:20:32 +0530

    added praccase-12.sh

---
+++ praccase-2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+echo "Enter a number from 1 to 7"
+read n
+case $n in
+	1) echo "sunday"
+	;;
+	2) echo "monday"
+	;;
+	3) echo "tuesday"
+	;;
+	4) echo "wednesday"
+	;;
+	5) echo "thursday"
+	;;
+	6) echo "friday"
+	;;
+	7) echo "saturday"
+	;;
+esac
+
